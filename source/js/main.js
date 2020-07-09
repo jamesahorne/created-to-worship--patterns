a4f7a4d95ebef4c1e1f563e2fcf9bfc7aa490cf7
James Horne <james99h39@gmail.com>
2020-07-09 10:17:51 +0100

    ✅♻️ Add sidenav

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,10 @@
-// console.log('hey')
+const vw = window.innerWidth;
+
+if (vw < 768) {
+  const sidenav__nav = document.getElementById("sidenav__nav");
+  const sidenav__toggle = document.getElementById("sidenav__toggle");
+
+  sidenav__toggle.addEventListener("click", function () {
+    sidenav__nav.classList.toggle("sidenav__nav--opened");
+  });
+}
